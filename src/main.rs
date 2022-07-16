adc6795013f121e5f2ca1c2eece060486ad8a892
Pierre Paquin <p.paquin1016@laposte.net>
2022-07-16 08:27:19 +0200

    feat: setup window and 2d camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(
+            WindowDescriptor {
+                title: "dice of the dungeon -- gamejam".to_string(),
+                width: 600.,
+                height: 600.,
+                ..Default::default()
+            }
+        )
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(startup_system) // todo add startup system
+        .run();
+}
+
+fn startup_system(
+    mut commands: Commands
+) {
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
